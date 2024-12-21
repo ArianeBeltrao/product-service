328f6d0b4c0b3f397648038b92cf5e8cfb19c750
Evandro Ladislau <HR6550@engie.com>
2024-12-21 15:19:54 -0300

    :construction: Create Produt Model

--- model/product-model.py
+++ model/product-model.py
@@ -1,16 +1,16 @@
 from datetime import datetime 
 
 class Product:
-    def __init__(self, id: str, name: str, price: float, quantity: int, description: str, active: bool = True, created_at: datetime = datetime, updated_at: datetime = None):
+    def __init__(self, id: str, name: str, price: float, quantity: int, description: str, active: bool = True, created_at: datetime = None, updated_at: datetime = None):
         self.id = id
         self.name = name
         self.price = price
         self.quantity = quantity
         self.active = active
-        self.descrition = description
-        self.created_at = created_at
+        self.description = description
+        self.created_at = created_at or datetime.now()
         self.updated_at = updated_at
         
 
     def __repr__(self):
-        return f"Product(id={self.id}, name='{self.name}', price={self.price}, quantity={self.quantity},)"
+        return f"Product(id={self.id}, name='{self.name}', price={self.price}, quantity={self.quantity}, active={self.active}, description={self.description}, created_at={self.created_at}, updated_at={self.updated_at})"
