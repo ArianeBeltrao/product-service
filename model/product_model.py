4750dc4a108408f9d0a8ee60296d1a1b60525b39
Evandro Ladislau <HR6550@engie.com>
2024-12-22 11:26:21 -0300

    :construction: Create Product  Model

--- model/product_model.py
+++ model/product_model.py
@@ -9,13 +9,10 @@ class Product(BaseModel):
         quantity: int
         active: bool = True
         description: str
-        created_at: Optional[datetime] = None
-        updated_at: Optional[datetime] = None
+        created_at: datetime = Field(default=None)
+        updated_at: datetime = Field(default=None)
         @model_validator(mode='before')
         def set_created_at(cls, values):
             if values.get('created_at') is None:
                 values['created_at'] = datetime.now()
             return values
-              
-        def __repr__(self):
-            return f"Product(id={self.id}, name='{self.name}', price={self.price}, quantity={self.quantity}, active={self.active}, description={self.description}, created_at={self.created_at}, updated_at={self.updated_at})"
