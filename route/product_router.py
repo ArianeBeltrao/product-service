b0bcb83aab9ae9dcbfcead6f424751e3a3f1c29c
Evandro Ladislau <evandroladislau23@gmail.com>
2024-12-24 00:04:59 -0300

    :construction: Create update endpoint

--- route/product_router.py
+++ route/product_router.py
@@ -24,7 +24,7 @@ async def create_product(product: Product):
     logger.info(f"CreateProduct request finished with response={product.model_dump()}")
     return product_created
 
-@router.put("/products/{product_id}")
+@router.put("/products/{product_id}", response_model=Product)
 async def update_product(product_id: str, product: Product):
     logger.info(f"Started UpdateProduct with body={product.model_dump()} and product_id={product_id}")
     product_updated = service.update_product(product_id, product)
