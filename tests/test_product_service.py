37ceec741de1ee1d090153f35bfe7c18b40e8917
Ariane <ariane@globalfy.com>
2025-01-05 12:43:54 -0300

    feat: added assert calls tests

--- tests/test_product_service.py
+++ tests/test_product_service.py
@@ -1,68 +1,89 @@
 from psycopg2 import DatabaseError
 import pytest
+from pytest import fixture
 from services.product_service import ProductService
 from unittest.mock import MagicMock
 
-mock_storage = MagicMock()
-service = ProductService(mock_storage)
+@fixture
+def mock_storage():
+    return MagicMock()
 
-def test_get_all_products(product):
+@fixture
+def service(mock_storage):
+    return ProductService(mock_storage)
+
+
+def test_get_all_products(product, mock_storage, service):
     mock_storage.get_all_products.return_value = [product]
     
     result = service.get_all_products()
     assert result == [product]
     mock_storage.get_all_products.assert_called_once()
 
-def test_get_product_by_id(product) -> None:
+def test_get_product_by_id(product, mock_storage, service) -> None:
     mock_storage.get_product_by_id.return_value = product
     
     result = service.get_product_by_id('01JFTE35ZRRZWCSKK6TBB1DZCT')
     assert result == product
+    mock_storage.get_product_by_id.assert_called_once_with('01JFTE35ZRRZWCSKK6TBB1DZCT')
     
-def test_create_product(product):
+def test_create_product(product, mock_storage, service):
     mock_storage.create_product.return_value = product
     
     result = service.create_product(product)
     assert result == product
+    mock_storage.create_product.assert_called_once_with(product)
     
-def test_update_product(product):
+def test_update_product(product, mock_storage, service):
     mock_storage.update_product.return_value = product
     
     result = service.update_product(product)
     assert result.updated_at != None
+    mock_storage.update_product.assert_called_once_with(product)
     
-def test_delete_product():
+def test_delete_product(mock_storage, service):
     mock_storage.delete_product_by_id.return_value = None
     result = service.delete_product_by_id('01JFTE35ZRRZWCSKK6TBB1DZCT')
     assert result == None
+    mock_storage.delete_product_by_id.assert_called_once_with('01JFTE35ZRRZWCSKK6TBB1DZCT')
     
-def test_get_all_products_value_error():
+def test_get_all_products_database_error(mock_storage, service):
     mock_storage.get_all_products.side_effect = DatabaseError()
     
     with pytest.raises(DatabaseError):
         service.get_all_products()
+        
+    mock_storage.get_all_products.assert_called_once()
     
-def test_get_product_by_id_value_error() -> None:
+def test_get_product_by_id_value_error(mock_storage, service) -> None:
     mock_storage.get_product_by_id.side_effect = ValueError()
     
-    with pytest.raises(ValueError): #assert se a exception valueerror for jogada
+    with pytest.raises(ValueError):
         service.get_product_by_id('01JFTE35')
         
-def test_create_product_value_error(product):
+    mock_storage.get_product_by_id.assert_called_once_with('01JFTE35')
+        
+def test_create_product_value_error(product, mock_storage, service):
     mock_storage.create_product.side_effect = DatabaseError()
     
     with pytest.raises(DatabaseError):
         service.create_product(product)
         
-def test_update_product_value_error(product):
+    mock_storage.create_product.assert_called_once_with(product)
+        
+def test_update_product_value_error(product, mock_storage, service):
     mock_storage.update_product.side_effect = ValueError()
     
     with pytest.raises(ValueError):
         service.update_product(product)
         
-def test_delete_product_by_id_value_error():
+    mock_storage.update_product.assert_called_once_with(product)
+        
+def test_delete_product_by_id_value_error(mock_storage, service):
     mock_storage.delete_product_by_id.side_effect = ValueError()
     
     with pytest.raises(ValueError):
         service.delete_product_by_id('01JFTE35')
+        
+    mock_storage.delete_product_by_id.assert_called_once_with('01JFTE35')
     
--- tests/test_product_storage.py
+++ tests/test_product_storage.py
@@ -19,6 +19,15 @@ def test_get_all_products(mock_cursor, storage, product):
     
     result = storage.get_all_products()
     assert result == [product]
+    
+    mock_cursor.execute.assert_called_once_with
+    (
+        """
+            SELECT id, name, description, price, quantity, active, created_at, updated_at
+            FROM products
+            WHERE active = True 
+        """
+    )
 
 def test_get_product_by_id(mock_cursor, storage, product):
     mock_cursor.fetchone.return_value = (
@@ -35,10 +44,27 @@ def test_get_product_by_id(mock_cursor, storage, product):
     result = storage.get_product_by_id('01JFTE35ZRRZWCSKK6TBB1DZCT')
     assert result == product
     
-def test_create_product(storage, product):
+    mock_cursor.execute.assert_called_once_with
+    (
+        """
+        SELECT id, name, description, price, quantity, active, created_at, updated_at
+        FROM products
+        WHERE id = %s;
+        """, ('01JFTE35ZRRZWCSKK6TBB1DZCT',)
+    )
+    
+def test_create_product(mock_cursor, storage, product):
     result = storage.create_product(product)
 
     assert result == product
+    
+    mock_cursor.execute.assert_called_once_with
+    (
+    """
+        INSERT INTO products (id, name, description, price, quantity, active, created_at)
+        VALUES (%s, %s, %s, %s, %s, %s, %s);
+    """, (product.id, product.name, product.description, product.price, product.quantity, product.active, product.created_at)
+    )
 
 def test_update_product(mock_cursor, storage, product):
     product.updated_at = datetime(2025, 12, 23, 15, 57, 25, 496623)
@@ -57,33 +83,76 @@ def test_update_product(mock_cursor, storage, product):
     result = storage.update_product(product)
     assert result == product
     
+    mock_cursor.execute.assert_called_once_with
+    (
+        """
+            UPDATE products
+            SET 
+                name = %s,
+                description = %s,
+                price = %s,
+                quantity = %s,
+                active = %s,
+                updated_at = %s
+            WHERE id = %s
+            RETURNING id, name, description, price, quantity, active, created_at, updated_at
+        """, (product.name, product.description, product.price, product.quantity, product.active, product.updated_at, product.id)
+    )
+    
 def test_delete_product_by_id(mock_cursor, storage):
     mock_cursor.rowcount = 1
     
     result = storage.delete_product_by_id('01JFTE35ZRRZWCSKK6TBB1DZCT')
     assert result == None
     
+    mock_cursor.execute.assert_called_once_with
+    (
+        """
+            DELETE FROM products
+            WHERE id = %s;
+        """, ('01JFTE35ZRRZWCSKK6TBB1DZCT',)   
+    )
+    
 def test_get_all_products_database_error(mock_cursor, storage):
     mock_cursor.execute.side_effect = DatabaseError()
     
     with pytest.raises(DatabaseError):
         storage.get_all_products()
+        
+    mock_cursor.execute.assert_called_once()
+    mock_cursor.fetchall.assert_not_called()
 
 def test_get_product_by_id_value_error(mock_cursor, storage):
     mock_cursor.fetchone.return_value = None
     
     with pytest.raises(ValueError):
         storage.get_product_by_id('01JFTE35ZRRZWCSKK6TBB1DZCT')
+    
+    mock_cursor.execute.assert_called_once()
+    mock_cursor.fetchone.assert_called_once()
         
 def test_create_product_database_error(mock_cursor, storage, product):
     mock_cursor.execute.side_effect = DatabaseError()
     
     with pytest.raises(DatabaseError):
         storage.create_product(product)
+
+    mock_cursor.execute.assert_called_once()
         
 def test_update_product_value_error(mock_cursor, storage, product):
-    mock_cursor.execute.side_effect = ValueError()
+    mock_cursor.fetchone.return_value = None
     
     with pytest.raises(ValueError):
         storage.update_product(product)
+
+    mock_cursor.execute.assert_called_once()
+    mock_cursor.fetchone.assert_called_once()
+        
+def test_delete_product_by_id(mock_cursor, storage):
+    mock_cursor.rowcount = 0
+    
+    with pytest.raises(ValueError):
+        storage.delete_product_by_id('01JFTE35')
+
+    mock_cursor.execute.assert_called_once()
         
